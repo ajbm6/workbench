adb93ac222959ff8804d96d5ed32e575aba3d389
alevento <alessandro.manneschi@gmail.com>
2016-08-30 16:42:51 +0200

    Test package

--- src/WorkbenchVersion.php
+++ src/WorkbenchVersion.php
@@ -205,7 +205,7 @@ EOF;
 
         //$TreadCopy = new WorkbenchCopyThread($this->BASE_PATH,"y:/semver/original/");
         $this->line('inizio copia');
-        $fi = new \FilesystemIterator($this->BASE_PATH, \FilesystemIterator::SKIP_DOTS);
+        $fi = new \GlobIterator($this->BASE_PATH, \FilesystemIterator::SKIP_DOTS);
         printf("There were %d Files", iterator_count($fi));
         $dir = new \DirectoryIterator($this->BASE_PATH);
         $file = new \FilesystemIterator($this->BASE_PATH);
