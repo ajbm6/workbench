bc10ae3c06d81672b2ec4719af79770fc45f4904
alevento <a@a.it>
2016-11-01 13:02:17 +0100

    fix

--- tests/WorkbenchVersionTest.php
+++ tests/WorkbenchVersionTest.php
@@ -10,6 +10,8 @@ namespace Padosoft\Workbench\Test;
 
 
 use Illuminate\Support\Facades\Artisan;
+use Symfony\Component\Process\ExecutableFinder;
+use Config;
 
 class WorkbenchVersionTest extends \Padosoft\LaravelTest\TestBase
 {
@@ -26,7 +28,21 @@ class WorkbenchVersionTest extends \Padosoft\LaravelTest\TestBase
     /** @test */
     public function testHardWorkCreateNoOk()
     {
+        $finder = new ExecutableFinder();
+        $finder->addSuffix('phar');
 
+        /*$this->gitBinary = '"'.str_replace("\\","/",$finder->find('git',null,[Config::get('workbench.git_path')])).'"';
+        if (!$this->gitBinary) {
+            throw new GitException('Unable to find the Git executable.');
+        }
+        $this->pharSamiBinary = '"'.str_replace("\\","/",$finder->find('sami',null,[Config::get('workbench.common_dev_lib_path')])).'"';
+        if (!$this->pharSamiBinary) {
+            throw new Exception('Unable to find the Sami phar.');
+        }
+        echo $this->pharSamiBinary;*/
+
+
+        /*
         $dir="Y:/Public/laravel-packages/www/laravel/5.2.x/packages/Padosoft/workbench/";
         $user="alevento";
         $password=env('PWD_ALE_GITHUB');
@@ -37,7 +53,7 @@ class WorkbenchVersionTest extends \Padosoft\LaravelTest\TestBase
             '--user'=>$user,
             '--password'=>$password,
             '--email'=>$email,
-        ]);
+        ]);*/
 
     }
 
